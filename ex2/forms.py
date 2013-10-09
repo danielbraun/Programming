d91354bd0cb5771f50aa8302e66c6934ac7f32b3
Daniel Braun <dbraun86@gmail.com>
2013-10-09 19:35:44 +0000

    some form improvements

--- ex2/forms.py
+++ ex2/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 
 class SortingForm(forms.Form):
-    input = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}),
-                            initial='10, 2.5, 5, -2',
-                            help_text='Comma separated numbers',
-                            )
-    output = forms.CharField(widget=forms.Textarea(attrs={'rows': 2}))
+    input = forms.CharField(
+        widget=forms.Textarea(attrs={'rows': 2, 'autofocus': 'autofocus'}),
+        initial='10, 2.5, 5, -2',
+        help_text='Comma separated numbers',
+    )
+    output = forms.CharField(widget=forms.Textarea(attrs={'rows': 2,
+                                                          'readonly': 'readonly'}))
